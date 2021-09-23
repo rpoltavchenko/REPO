99ec312093df03d98a1cac6572f3387825f50736
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-09-23 17:44:23 +0300

    Create task8.js

---
+++ task8.js
@@ -0,0 +1,20 @@
+let firstCount = 3600;
+let hoursInDay = 24;
+let hour = 1;
+let dayisInMonth = 30;
+
+let secondsInHour = hour * firstcount;
+let secondsInDay = secoundsInHour*hoursInDay;
+let secondsinMonth = secondsInDay*dayisInMonth;   
+
+console.log(secondsInHour, secondsInDay, secondsinMonth);
+
+or 
+
+// let hour = 1;
+
+// let secondsInHour = hour * 3600;
+// let secondsInDay = secoundsInHour*24;
+// let secondsInMonth = secondsInDay*30;   
+
+// console.log(secondsInHour, secondsInDay, secondsInMonth);
