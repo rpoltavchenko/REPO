8c35084fbe3982a4f98f8882a6733f054fe51ea2
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-09-26 11:00:55 +0300

    Create task6.js

---
+++ Homework1/task6.js
@@ -0,0 +1,13 @@
+const userName = "Roman";
+const userSurname = "Poltavchenko";
+const groupNumber = "Lv-636.JS Core21";
+const birthYear = 1990;
+
+let isMarried = confirm ("Are you married?");
+alert(isMarried);
+
+console.log(typeof birthYear, isMarried, userName);
+
+let nullVariable = null;
+let x;
+console.log (typeof nullVariable, x);
