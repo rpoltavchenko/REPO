39f7eb6186d6fcd45c66d7dec4761f655f5b986a
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-09-30 17:23:14 +0300

    Create task2.js

---
+++ Homework2/task2.js
@@ -0,0 +1,8 @@
+let num = prompt("Enter your number");
+if (num > 0 && num % 2){ console.log ('Your number is valid')}
+   }
+ else if (num % 7 === 0) {console.log ('Your number is VALID and divides on 7')}
+
+else {
+alert ('Try again')    
+}
