6d966e5d953ba4d3d7d2729d3c66267c2e26cc2d
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-10-04 07:22:28 +0300

    Create task1.js

---
+++ Homework2/task1.js
@@ -0,0 +1,17 @@
+let x = 1;
+let y = 2;
+let res = x*10 + y + "";
+console.log(res);
+console.log(typeof res);
+
+let res2 = true + (x*y+"");
+console.log(res2);
+console.log(typeof res2);
+
+let res3 = (x+y)>0;
+console.log(res3)
+console.log(typeof res3);
+
+let res4 = (x+y)/res2;
+console.log(res4);
+
