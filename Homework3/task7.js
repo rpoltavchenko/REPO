c618834e9d4fec22e5141c6fcccaf824a973c2c0
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-10-04 17:47:37 +0300

    Update task7.js

--- Homework3/task7.js
+++ Homework3/task7.js
@@ -1,14 +1,9 @@
-7. Напишіть функцію, яка повертає останній елемент масиву. Функція може приймати 2 параметра: 
-1-ий масив, 2-ий числовий параметр, що відповідає кількості 'х' останніх елементів масиву, які треба вивести.
-
-Sample Output:
-console.log(lastElem([3, 4, 10, -5]));      // -5
-console.log(lastElem([3, 4, 10, -5],2));   // [10, -5]
-console.log(lastElem([3, 4, 10, -5],8));   // [3, 4, 10, -5]
-
-
-
-function last(arr, i) {
-    let i = i || 0;
-    return arr[arr.length - (1 + i)];
+let arr = [2, 4, 6, 8, 10, 12, 14, 16];
+function getLastItem(arr,b) {
+    let lastItem = arr[arr.length-1];
+    result = arr.slice(-b)
+   if (b <=1 || !b){
+       return lastItem
+    } else return result
 }
+  
