8b7702f923feb8c4ff6b08f92a8e63429bc943c7
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-10-04 17:47:22 +0300

    Create task1.js

---
+++ Homework3/task1.js
@@ -0,0 +1,19 @@
+// Заданий масив з елементами [2, 3, 4, 5]. 
+// За допомогою циклу for знайдіть добуток елементів цього масиву. Також реалізуйте завдання через while.
+
+let arr = [2,3,4,5];
+let total = 1;
+
+for (let i = 0; i < arr.length; i++) {
+    total *= arr[i];
+} 
+
+console.log(total);
+
+let i = 0;
+while (i<arr.length) { 
+    total*=arr[i];
+    console.log(total);
+    i++;   
+}
+
