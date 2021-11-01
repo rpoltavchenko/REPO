30a954099c5e78d74fee68e90d56b1c79c93abea
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-11-01 16:55:16 +0200

    Delete task7.js

--- task7.js
+++
@@ -1,4 +0,0 @@
-let login = prompt("What is your login?", "user")
-let email = prompt("What is your email?", "user@gmail.com")
-let password = prompt("What is your password?", "qwerty")
-alert("Dear " + login + " your " + "e-mail " + "is " + email + " your " + "password " + "is " + password);
