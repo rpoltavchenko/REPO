df23392ce9ddcbf0c19164e6be09d078e991d46d
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-11-01 16:54:23 +0200

    Create task2.js

---
+++ Homework5/task2.js
@@ -0,0 +1,11 @@
+const currentObj = {
+    currentGrandPrix:'Monaco', 
+    theLastPoleSitter: 'LeClerc', 
+    theLastWinner: 'Verstappen', 
+    lastTeamWinner: "RedBull", 
+    totalLaps: 78};
+
+function showProps(obj) {
+console.log (Object.keys(obj));
+console.log (Object.values(obj));
+}
