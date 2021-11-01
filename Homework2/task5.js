282af9b7a9486bc60f76f75ad56c36055f73583d
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-11-01 16:54:15 +0200

    Update task5.js

--- Homework2/task5.js
+++ Homework2/task5.js
@@ -2,13 +2,8 @@ let isAdult = prompt ("Are you 18+?");
 
 if (isAdult >= 18 && isAdult < 90) {
     alert ('You are adult, you can pass')
-<<<<<<< HEAD
 } else if (isAdult >= 0 && isAdult < 18) {
     alert ('Apologies, you are too young')
-=======
-} else if (isAdult >= 0 && isAdult <= 18) {
-    alert ('apologies, you are too young')
->>>>>>> d042331cf024fb518766a0c9aaf52e8bae097495
 }
 
 else {
