730d8987241a3cfbf1cee9bf4c26223fac4a8454
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-11-04 23:27:55 +0200

    Update task4.js

--- Homework6/task4.js
+++ Homework6/task4.js
@@ -1,4 +1,20 @@
-const parentEl = document.querySelector('ul');
-const firstEl = parentEl.childNodes[1].innerHTML;
+const list = document.querySelector('ul');
+const firstEl = list.children[0].innerHTML;
+const lastEl = list.children[4].innerHTML;
+const secondEl = list.children[1].innerHTML;
+const forthEl = list.children[3].innerHTML;
+const thirdEl = list.children[2].innerHTML;
 
-alert (firstEl);
+
+console.log (firstEl, lastEl, secondEl, forthEl, thirdEl);
+
+// const list = document.getElementById('list');
+// const firstEl = list.firstElementChild.innerHTML;
+// const lastEl = list.lastElementChild.innerHTML;
+// const secondEl = list.firstEl.nextSibling.innerHTML;
+// const forthEl = list.lastEl.previousSibling.innerHTML;
+// const thirdEl = list.forthEl.previousSibling.innerHTML;
+
+// console.log (firstEl, lastEl, secondEl, forthEl, thirdEl);
+
+
