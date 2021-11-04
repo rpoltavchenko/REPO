0039d8c196c919f2551ab34d2df599bd5db3a02b
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-11-04 23:27:52 +0200

    Create task5.js

---
+++ Homework6/task5.js
@@ -0,0 +1,11 @@
+let newHeader = document.querySelector('h1').style.backgroundColor = 'lightgreen';
+let firstPar = document.querySelector('p').style.fontWeight = 1000;
+let secondPar = document.getElementById('myDiv').children[1].style.color = 'red';
+let thirdPar = document.getElementById('myDiv').children[2].style.textDecoration = 'underline';
+let fourthPar = document.getElementById('myDiv').lastElementChild.style.fontStyle = 'italic';
+let firstList = document.querySelector('ul').style.listStyle = 'none';
+let newFirstList = document.querySelector('ul').style.display = 'flex';
+let newSpan = document.querySelector('span').style.display = 'none';
+
+
+
