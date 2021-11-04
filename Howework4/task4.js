195a4c9b38848845deaeb840a8d9a4db756241ac
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-11-04 16:15:11 +0200

    Add files via upload

--- Howework4/task4.js
+++ Howework4/task4.js
@@ -1,36 +1,73 @@
-// 4. Реалізуйте функцію showUser(id), яка приймає параметром користувацьке id і повертає об’єкт, 
-// який містить значення переданої id. Також функція викидає помилку у разі якщо введено від’ємне id.
-	
-// Реалізуйте функцію showUsers(ids), яка приймає параметром масив користувацьких айді ids, 
-// перевіряє з використанням функції showUser() кожен елемент масиву ids на коректність, 
-// в разі виключної ситуації виводить повідомлення про помилку. Функція showUsers(ids) повертає масив об’єктів, 
-// де значеннями ключів є коректні елементи ids.
+class Worker {
+    #exp = 1.2;
+    constructor(fullName, dayRate, workingDays){
+        this.fullName = fullName;
+        this.dayRate = dayRate;
+        this.workingDays = workingDays;
+    }
+    //declare methods via object literals
+showSalary(){
+    const {fullName, dayRate, workingDays} = this;
+    console.log(`${fullName}'s salary is: ${dayRate * workingDays}`);
+    }
+showSalaryWithExp(){
+    const {fullName, dayRate, workingDays} = this;
+    console.log(`${fullName}'s salary is: ${dayRate * workingDays * this.#exp}`);
+    }
+showWorkerSalary(){
+    const {dayRate, workingDays} = this;
+    return (`${dayRate * workingDays * this.#exp}`);
+    }
 
-// 	Приклад роботи програми:
-// showUsers([7, -12, 44, 22]);
-// Error: ID must not be negative: -12
-// [ {id: 7}, {id: 44}, {id: 22} ]
+//getter and seter to change #exp value
 
-function ShowUser(id) {
-        if (id<0) {
-            throw new Error ('Invalid ID, ID should be positive ' + id)
-    } return {id:id};  
+get showExp() {
+    return this.#exp;
+    }
+set setExp(exp){
+    this.#exp = exp;
 }
 
-function showUsers(ids) {
-    let result = [];
-    ids.forEach((id) => {
-        try {
-            let person = showUser(id);
-            result.push(person);
-        } catch (exception) {
-            console.log(exception.msg);
-        }
-        });
 
-        return result;
+
+sortSalaries(workersArr){
+    let sortedSalary = workersArr.sort(function(x,y){
+        return x.showWorkerSalary() - y.showWorkerSalary();
+    })
+    for (let worker of sortedSalary){
+        console.log(worker.fullName + ": " + list.showWorkerSalary());
+    }
+} 
+
 }
 
-showUsers([1, 2, -10, -22, 50]);
+let worker1 = new Worker("Peter Parker", 20, 23);
+console.log(worker1.fullName);                 
+worker1.showSalary();
+console.log("New experience: " + worker1.showExp);
+worker1.showSalaryWithExp();
+worker1.setExp = 1.5;
+console.log("New experience: " + worker1.showExp);
+worker1.showSalaryWithExp();
 
+let worker2 = new Worker("Reed Richards", 16, 23);
+console.log(worker1.fullName);                 
+worker1.showSalary();
+console.log("New experience: " + worker1.showExp);
+worker1.showSalaryWithExp();
+worker1.setExp = 1.5;
+console.log("New experience: " + worker1.showExp);
+worker1.showSalaryWithExp();
+
+let worker3 = new Worker("Tony Stark", 10, 40);
+console.log(worker1.fullName);                 
+worker1.showSalary();
+console.log("New experience: " + worker1.showExp);
+worker1.showSalaryWithExp();
+worker1.setExp = 1.5;
+console.log("New experience: " + worker1.showExp);
+worker1.showSalaryWithExp();
+
+
+const workersArr = [worker1, worker2, worker3];
 
