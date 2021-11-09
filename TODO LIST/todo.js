f5f18459b4ab0e4d02c9e490c5a7259b3fb6cb63
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-11-09 23:44:01 +0200

    Update todo.js

--- TODO LIST/todo.js
+++ TODO LIST/todo.js
@@ -1,29 +1,62 @@
-// const form  = document.querySelector('#form');
-// const todos = document.querySelector('#todos');
-// const input = document.querySelector('#input');
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-//     const todoText = input.value;
-    
-//     if(todoText){
-//         newTodo = document.createElement ('li');
-//         newTodo.innerText = todoText;
-
-//         newTodo.addEventListener ('click', () => {
-//             newTodo.classList.toggle ('completed');
-//         });
-
-//         todos.appendChild(newTodo);
-//         input.value = "";
-//     }
-// });
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
+const title = document.querySelector('.title');
 
-const todos = JSON.parse(localStorage.getItem("todos"));
+let activeListName = 'todos'
+
+let todos = JSON.parse(localStorage.getItem(activeListName));
+
+const newList = document.querySelector('#new-list')
+const btn = document.querySelector('#btn')
+const listOfTodoEl = document.querySelector('#list-of-todo')
+
+const listOfTodo = Object.keys(window.localStorage)
+
+listOfTodo.forEach(element => { 
+    const newListElem = document.createElement("li");
+newListElem.innerText = element;
+listOfTodoEl.appendChild(newListElem);
+
+
+newListElem.addEventListener('click',() => {
+title.innerText = newListElem.innerText
+activeListName = newListElem.innerText
+todos = JSON.parse(localStorage.getItem(activeListName));
+todosUL.innerHTML = '';
+if (todos) {
+    todos.forEach((todo) => {
+        addTodo(todo);
+    });
+}
+});
+    
+});
+btn.addEventListener("click", () => {
+    localStorage.setItem(newList.value, JSON.stringify([]));
+    
+    const newListElem = document.createElement("li");
+    newListElem.innerText = newList.value;
+    listOfTodoEl.appendChild(newListElem);
+    newList.value = "";
+    
+    newListElem.addEventListener('click',() => {
+    title.innerText = newListElem.innerText
+    activeListName = newListElem.innerText
+    todos = JSON.parse(localStorage.getItem(activeListName));
+    todosUL.innerHTML = '';
+    if (todos) {
+        todos.forEach((todo) => {
+            addTodo(todo);
+        });
+    }
+});
+
+
+});
+
+
+
 
 if (todos) {
     todos.forEach((todo) => {
@@ -46,6 +79,7 @@ function addTodo(todo) {
 
     if (todoText) {
         const todoEl = document.createElement("li");
+        todoEl.classList.add("todo-item");
         if (todo && todo.completed) {
             todoEl.classList.add("completed");
         }
@@ -75,7 +109,7 @@ function addTodo(todo) {
 }
 
 function updateLS() {
-    const todosEl = document.querySelectorAll("li");
+    const todosEl = document.querySelectorAll(".todo-item");
 
     const todos = [];
 
@@ -86,5 +120,13 @@ function updateLS() {
         });
     });
 
-    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem(activeListName, JSON.stringify(todos));
 }
+
+
+
+// todosUL.addEventListener('click', (event)=>{
+//     event.target.classList.toggle("completed");
+//             updateLS();
+    
+// })
