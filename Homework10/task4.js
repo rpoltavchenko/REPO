b832bd6e0cc1fe1d820139b5f8cf22215abf86cb
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-11-14 14:44:00 +0200

    Create task4.js

---
+++ Homework10/task4.js
@@ -0,0 +1,28 @@
+let server = {
+    data: 0,
+    convertToString: function (callback) {
+        callback(() => {
+            return this.date + "";
+        });
+
+    }
+}
+
+let client = {
+    server: server,
+    result: "",
+    calc: function (data) {
+        this.server.data = data;
+        this.server.convertToString(this.notification());
+    }, notification: function () {
+        return (callback => {
+            this.result = callback();
+        });
+    }
+};
+
+client.calc(123);
+console.log(client.result);
+console.log(typeof client.result);
+
+
