7ce8edb337479c6ea05a90c82e6418ac7e65a4ea
rpoltavchenko <91268288+rpoltavchenko@users.noreply.github.com>
2021-11-14 21:39:15 +0200

    Update task7.js

--- Homework8/task7.js
+++ Homework8/task7.js
@@ -1,6 +1,12 @@
 function checkLogin(inputData) {
     const expr = /^[a-zA-Z]{1}[a-zA-Z0-9\.]{1,9}$/;
-    let 
+    let needNumber = /[0-9\.]+/g;
+    let finalNumber = inputData.match(needNumber);
+    let result = expr.test(inputData);
 
-    
+    console.log(`The ${inputData} is ${result} - ${finalNumber}`);
 }
+
+checkLogin('ee1.1ret3');
+checkLogin('ee1*1ret3');
+
